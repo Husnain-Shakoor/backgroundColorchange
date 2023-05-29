4d28f507259892725311848aa59c54200648b5f1
Husnain-Shakoor <108575153+Husnain-Shakoor@users.noreply.github.com>
2023-05-30 00:23:51 +0500

    Initial commit

---
+++ background_color.js
@@ -0,0 +1,12 @@
+function hello() {
+  document.write("Turn Off the page now");
+}
+
+function changeBG(color) {
+  document.body.style.backgroundColor = color;
+}
+
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+
+console.log(a === b);
